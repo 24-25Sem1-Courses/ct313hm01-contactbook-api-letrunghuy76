dfb578d52680880da2252187a71d9717b25eede3
letrunghuy76 <letrunghuyks1323@gmail.com>
2024-09-16 16:56:03 +0700

    Define routes for managing contacts

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
-const express = require('express'); 
-const cors = require('cors'); 
-const app = express(); 
-app.use(cors()); 
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
-app.get('/', (req, res) => { 
-return res.json({ 
-message: 'ok' 
-}); 
-}); 
-module.exports = app; 
+const express = require('express');
+const cors = require('cors');
+const contactsRouter = require('./routes/contacts.router');
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+return res.json({
+message: 'ok'
+});
+});
+contactsRouter.setup(app);
+module.exports = app;
